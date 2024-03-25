e4dbb667a7d82aa005fad23a82ae2c298f24fa1d
Kshitij Anand <kshitijkumar2002@gmail.com>
2024-03-25 23:11:27 +0530

    feat/completed ui of gemini

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Sidebar from "./components/sidebar/sidebar";
+import Main from "./components/Main/Main";
 
 const App = () => {
   return(
     <>
       <Sidebar/>
+      <Main/>
     </>
   )
 }
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import './Main.css'
+import { assets } from '../../assets/assets'
+const Main = () =>{
+
+return (
+  <div className='main'>
+    <div className="nav">
+        <p>Gemini</p>
+        <img src={assets.user_icon} alt="" />
+    </div>
+    <div className="main-container">
+        <div className="greet">
+            <p><span>Hello, Kshitij.</span></p>
+            <p>How May I Assist You Today?</p>
+        </div>
+        <div className="cards">
+            <div className="card">
+                <p>Guide with some Amazing projects for my resume</p>
+                <img src={assets.compass_icon} alt="" />
+            </div>
+            <div className="card">
+                <p>Briefly summerize the concept:urban palnning</p>
+                <img src={assets.bulb_icon} alt="" />
+            </div>
+            <div className="card">
+                <p>Brain storm team bonding activities for our work retreat</p>
+                <img src={assets.message_icon} alt="" />
+            </div>
+            <div className="card">
+                <p>Improve the Reablity of follwing code</p>
+                <img src={assets.code_icon} alt="" />
+            </div>
+        </div>
+        <div className="main-bottom">
+            <div className="search-box">
+                <input type="text" placeholder='Enter the prompt here...'/>
+                <div>
+                    <img src={assets.gallery_icon} alt="" />
+                    <img src={assets.mic_icon} alt="" />
+                    <img src={assets.send_icon} alt="" />
+                </div>
+            </div>
+            <p className="bottom-info">
+                Altough a Gemini clone has been developed by Kshitij Still it may display incorrect info.
+            </p>
+        </div>
+    </div>
+  </div>
+ )
+}
+
+  export default Main
