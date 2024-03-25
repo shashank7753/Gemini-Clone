7f04431f02d0effd039a1b3114b7b11cc6a64d8c
Kshitij Anand <kshitijkumar2002@gmail.com>
2024-03-25 17:31:28 +0530

    feat/added collapsable sidebar and changed font

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,17 +1,46 @@
-import React from "react";
-import './Sidebar.css'
-import {assets} from '../../assets/assets'
+import React, { useState } from "react";
+import "./Sidebar.css";
+import { assets } from "../../assets/assets";
 
 const Sidebar = () => {
-  return(
+  const [extended, setExtended] = useState(false);
+
+  return (
     <div className="sidebar">
       <div className="top">
-        <img src={assets.menu_icon} alt="" />
+        <img onClick={()=>setExtended(prev=>!prev)} className="menu" src={assets.menu_icon} alt="" />
+        <div className="new_chat">
+          <img src={assets.plus_icon} alt="" />
+          {extended ? <p>New Chat</p> : null}
+        </div>
+
+        {extended ? (
+          <div className="recent">
+            <p className="recent-title">Recent</p>
+            <div className="recent-entry">
+              <img src={assets.message_icon} alt="" />
+              <p>What is react.... </p>
+            </div>
+          </div>
+        ) : null}
       </div>
+
       <div className="bottom">
+        <div className="bottom-items recent-entry">
+          <img src={assets.question_icon} alt="" />
+          {extended?<p>Help</p>:null}
+        </div>
+        <div className="bottom-items recent-entry">
+          <img src={assets.history_icon} alt="" />
+          {extended?<p>Activity</p>:null}
+        </div>
+        <div className="bottom-items recent-entry">
+          <img src={assets.setting_icon} alt="" />
+          {extended?<p>Setting</p>:null}
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Sidebar;
