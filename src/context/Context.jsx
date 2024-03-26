1fb53fd6a219725fbf590b276a858848b0bcc789
Kshitij Anand <kshitijkumar2002@gmail.com>
2024-03-26 09:54:01 +0530

    feat/made input feild to controlled input feild

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,15 +1,35 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import runChat from "../config/gemini";
 
 
 export const Context=createContext();
 const ContextProvider=(props)=>{
 
+
+    const [input,setInput]=useState("");
+    const [recentPrompt,setRecentPrompt]=useState("");
+    const [prevPrompts,setPrevPrompts]=useState([]);
+    const [showResult,setShowResult]=useState(false);
+    const [loading,setLoading]=useState(false);
+    const [resultData,setResultData]=useState("");
+
+
     const onSent=async(prompt)=>{
-      await  runChat(prompt)
+      await  runChat(input)
     }
-        onSent("What is react?")
+     
     const Contextvalue={
+
+        prevPrompts,
+        setPrevPrompts,
+        onSent,
+        setRecentPrompt,
+        recentPrompt,
+        showResult,
+        loading,
+        resultData,
+        input,
+        setInput
 
 
     }
