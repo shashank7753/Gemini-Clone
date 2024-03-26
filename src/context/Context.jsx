e5c8e9b97d7ee5280ff97891ae3b3f59d5d9df1c
Kshitij Anand <kshitijkumar2002@gmail.com>
2024-03-26 12:56:49 +0530

    feat/trying to add typing effect

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -12,6 +12,14 @@ const ContextProvider=(props)=>{
     const [showResult,setShowResult]=useState(false);
     const [loading,setLoading]=useState(false);
     const [resultData,setResultData]=useState("");
+    const delayPara= (index,nextWord)=>{
+        setTimeout(function(){
+
+            setResultData(prev=>prev+nextWord)
+
+        },75*index)
+
+    }
 
 
     const onSent=async(prompt)=>{
@@ -20,7 +28,19 @@ const ContextProvider=(props)=>{
         setShowResult(true)
         setRecentPrompt(input)
       const response = await  runChat(input)
-      setResultData(response)
+      let responseArray=response.split("**");
+      let newResponse
+      for(let i=0; i<responseArray.length;i++){
+
+        if(i ===0 ||i%2!=1){
+            newResponse+=responseArray[i];
+
+        }
+        else{
+            newResponse+="<b>"+responseArray[i]+"</b>"
+        }
+      }
+      setResultData(newResponse)
       setLoading(false)
       setInput("")
     }
