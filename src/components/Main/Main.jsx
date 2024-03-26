9dc775d665514d18c01f4463237a01ba0ffa3228
Kshitij Anand <kshitijkumar2002@gmail.com>
2024-03-26 14:28:29 +0530

    completed

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,84 +1,96 @@
-import React, { useContext } from 'react'
-import './Main.css'
-import { assets } from '../../assets/assets'
-import { Context } from '../../context/Context'
-const Main = () =>{
-    const {onSent,recentPrompt,showResult,loading,resultData,setInput,input}=useContext(Context)
-return (
-  <div className='main'>
-    <div className="nav">
+import React, { useContext } from "react";
+import "./Main.css";
+import { assets } from "../../assets/assets";
+import { Context } from "../../context/Context";
+const Main = () => {
+  const {
+    onSent,
+    recentPrompt,
+    showResult,
+    loading,
+    resultData,
+    setInput,
+    input,
+  } = useContext(Context);
+  return (
+    <div className="main">
+      <div className="nav">
         <p>Gemini</p>
-        <img src={assets.user_icon} alt="" />
-    </div>
-    <div className="main-container">
-     {!showResult
-        ?<>
-        <div className="greet">
-            <p><span>Hello, Kshitij.</span></p>
-            <p>How May I Assist You Today?</p>
-        </div>
-        <div className="cards">
-            <div className="card">
+        <img src={assets.formal_pic_gemini} alt="" />
+      </div>
+      <div className="main-container">
+        {!showResult ? (
+          <>
+            <div className="greet">
+              <p>
+                <span>Hello, Kshitij.</span>
+              </p>
+              <p>How May I Assist You Today?</p>
+            </div>
+            <div className="cards">
+              <div className="card">
                 <p>Guide with some Amazing projects for my resume</p>
                 <img src={assets.compass_icon} alt="" />
-            </div>
-            <div className="card">
+              </div>
+              <div className="card">
                 <p>Briefly summerize the concept:urban palnning</p>
                 <img src={assets.bulb_icon} alt="" />
-            </div>
-            <div className="card">
+              </div>
+              <div className="card">
                 <p>Brain storm team bonding activities for our work retreat</p>
                 <img src={assets.message_icon} alt="" />
-            </div>
-            <div className="card">
+              </div>
+              <div className="card">
                 <p>Improve the Reablity of follwing code</p>
                 <img src={assets.code_icon} alt="" />
+              </div>
             </div>
-        </div>
-        
-        </>
-        :
-        <div className='result'>
+          </>
+        ) : (
+          <div className="result">
             <div className="result-title">
-                <img src={assets.user_icon} alt="" />
-                <p>{recentPrompt}</p>
+              <img src={assets.user_icon} alt="" />
+              <p>{recentPrompt}</p>
             </div>
             <div className="resultData">
-                <img src={assets.gemini_icon} alt="" />
-                {loading?
-                <div className='loader'>
-                    <hr />
-                    <hr />
-                    <hr />
-
-                </div>:
-                
-                <p dangerouslySetInnerHTML={{__html:resultData}}></p>
-                }
-            </div>
-
-        </div>
-        
-        
-     }
-        
-        <div className="main-bottom">
-            <div className="search-box">
-                <input onChange={(e)=>setInput(e.target.value)} value={input} type="text" placeholder='Enter the prompt here...'/>
-                <div>
-                    <img src={assets.gallery_icon} alt="" />
-                    <img src={assets.mic_icon} alt="" />
-                    {input?<img onClick={()=>onSent()} src={assets.send_icon} alt="" />:null
-                    }
+              <img src={assets.gemini_icon} alt="" />
+              {loading ? (
+                <div className="loader">
+                  <hr />
+                  <hr />
+                  <hr />
                 </div>
+              ) : (
+                <p dangerouslySetInnerHTML={{ __html: resultData }}></p>
+              )}
             </div>
-            <p className="bottom-info">
-                Altough a Gemini clone has been developed by Kshitij Still it may display incorrect info.
-            </p>
-        </div>
-    </div>
-  </div>
- )
-}
+          </div>
+        )}
 
-  export default Main
+        <div className="main-bottom">
+          <div className="search-box">
+            <input
+              onChange={(e) => setInput(e.target.value)}
+              value={input}
+              type="text"
+              placeholder="Enter the prompt here..."
+            />
+            <div>
+              <img src={assets.gallery_icon} alt="" />
+              <img src={assets.mic_icon} alt="" />
+              {input ? (
+                <img onClick={() => onSent()} src={assets.send_icon} alt="" />
+              ) : null}
+            </div>
+          </div>
+          <p className="bottom-info">
+            Altough a Gemini clone has been developed by Kshitij Still it may
+            display incorrect info.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
